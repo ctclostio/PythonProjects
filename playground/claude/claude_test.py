1830b21a886c726a1a4319215c42655747367d21
ctclostio <trackerclostio@gmail.com>
2024-03-09 16:05:36 -0700

    Update claude_test.py

--- playground/claude/claude_test.py
+++ playground/claude/claude_test.py
@@ -2,7 +2,7 @@ import anthropic
 
 client = anthropic.Anthropic(
     # defaults to os.environ.get("ANTHROPIC_API_KEY")
-    api_key="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",
+    api_key="sk-ant-api03-daYPpClDtjV0BzFQZ8EVNL3MkgK1Ztxo0hMaK6iNuB5EaLkUHsoTCaPsepc_YOC-GggXFKpPp2PHKlqgXzF0Pw-NNRgdg",
 )
 
 message = client.messages.create(
